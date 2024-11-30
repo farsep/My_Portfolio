66dbfbd17525155e1d2127c7bec296e052f667b0
Farid <faridespinoza05@hotmail.com>
2024-11-29 21:27:34 -0500

    Initialization with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center mt-10 bg-red-100 text-red-200'>Hey Hello My name Kidd Keo</h1>
+    </>
+  )
+}
+
+export default App
